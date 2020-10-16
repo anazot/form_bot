fe19bcfc631456be06b99da3a64aeb2de0d9b3f0
anazot <70477865+anazot@users.noreply.github.com>
2020-10-17 00:19:29 +0300

    first

---
+++ src/components/Login.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <HelloWorld></HelloWorld>
+    <form>
+      <label>Логин</label>
+      <input required type="text" v-model="login">
+      <label>Пароль</label>
+      <input required type="text" v-model="password">
+      <button>Войти</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data(){
+    return{
+      login: "",
+      password: "",
+    }
+
+  },
+  computed:{
+
+  },
+  components:{
+
+  },
+  methods:{
+    signup: function (){
+      let login = this.login;
+      let password = this.password;
+      this.$store.dispatch('signup', {login, password}).then(() => {
+        this.$router.push('/')
+            .catch(error => {
+              console.log(error)
+            })
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
